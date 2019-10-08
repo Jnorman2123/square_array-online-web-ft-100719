d0ca5650fcd6f6c9cfcd09d53c217cf3fd5dcace
Jonathan Norman <jnorman2123@gmail.com>
2019-10-08 14:04:27 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,10 +1,10 @@
-def square_array(array)
-  new_array = []
-  array.each do |item|
-    new_array << item ** 2
-  end
-  new_array
-end
+# def square_array(array)
+#   new_array = []
+#   array.each do |item|
+#     new_array << item ** 2
+#   end
+#   new_array
+# end
 
 
 def square_array_with_collect(array)
